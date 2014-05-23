b1b74e95c738304bb6f5db2a8e2cf4cd0be319bd
ebrandner <brandner.erik@gmail.com>
2014-05-23 17:22:35 +0200

    NewReleasePrice updated + finished

--- NewReleasePrice.java
+++ NewReleasePrice.java
@@ -5,4 +5,11 @@ public class NewReleasePrice extends Price {
 		return Movie.NEW_RELEASE;
 	}
 	
+	double getCharge(int daysRented){
+		return daysRented * 3;
+	}
+	
+	int getFrequentRenterPoints(int daysRented) {
+		return (daysRented > 1) ? 2: 1;
+	}
 }
