ba70db484acca321135797522d4766848ab59f00
ebrandner <brandner.erik@gmail.com>
2014-05-23 17:19:08 +0200

    added new classes

---
+++ NewReleasePrice.java
@@ -0,0 +1,8 @@
+
+public class NewReleasePrice extends Price {
+	
+	int getPriceCode() {
+		return Movie.NEW_RELEASE;
+	}
+	
+}
---
+++ RegularPrice.java
@@ -0,0 +1,8 @@
+
+public class RegularPrice extends Price {
+	
+	int getPriceCode() {
+		return Movie.REGULAR;
+		}
+	
+}
